c88946818f33a00053dfd79eb8b2533b53396c81
ekinohito <zhenyagur9@gmail.com>
2021-12-04 01:49:57 +0300

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import { AppBar, Button, CircularProgress, Container, createTheme, Divider, Drawer, Grid, IconButton, Link, ScopedCssBaseline, Stack, Toolbar, Typography } from '@mui/material'
+import { AppBar, Button, CircularProgress, Container, createTheme, Divider, Drawer, Grid, IconButton, LinearProgress, Link, Paper, ScopedCssBaseline, Stack, Toolbar, Typography } from '@mui/material'
 import { Box, ThemeProvider } from '@mui/system'
 import { Menu, ArrowLeft } from '@mui/icons-material'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const theme = createTheme({
   palette: {
@@ -18,9 +18,39 @@ const theme = createTheme({
 })
 
 const Home: NextPage = () => {
+  const [numberSrc, setNumberSrc] = useState("/placeholder.png")
+  const [numberStatus, setNumberStatus] = useState<"ok" | "pending" | "error">("ok")
   const [frameSrc, setFrameSrc] = useState("/placeholder.png")
   const [frameStatus, setFrameStatus] = useState<"ok" | "pending" | "error">("ok")
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [time, setTime] = useState(100)
+  const [currentDate, setCurrentDate] = useState(new Date())
+
+  function updateFrame() {
+    setFrameSrc(`https://loremflickr.com/600/450?random=${Math.random()}`)
+    setFrameStatus("pending")
+  }
+
+  function resetFrame() {
+    updateFrame()
+    setTime(0)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(time => {
+        if (time >= 3000) {
+          updateFrame()
+          return 0
+        }
+        return time + 100
+      })
+      setCurrentDate(new Date())
+    }, 100)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
   return (
     <ScopedCssBaseline>
       <ThemeProvider theme={theme}>
@@ -33,9 +63,9 @@ const Home: NextPage = () => {
           <Container>
             <Toolbar>
               <IconButton onClick={() => setIsMenuOpen(isOpen => !isOpen)}>
-                <Menu />
+                <Menu color="disabled"/>
               </IconButton>
-              <Typography variant="h4">Accenture</Typography>
+              <Typography variant="h4" fontWeight="">5 Денег</Typography>
             </Toolbar>
           </Container>
         </AppBar> 
@@ -52,44 +82,81 @@ const Home: NextPage = () => {
           </Stack>
         </Drawer>
         <Container>
-          <Grid>
-
+          <Grid container>
+          <Grid item xs={8} textAlign="center" marginTop={4}>
+          <Typography variant="body1">
+            Зона А /
+            Плановое время: 12:00-13:00 /
+            Время сейчас: {currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds()}
+          </Typography>
+          </Grid>
+          <Grid item xs={8}>
+            <Stack spacing={2} marginTop={4} alignItems="center">
+              <Typography variant="h3">Кадры</Typography>
+              <Box position="relative">
+                <Image width={600} height={450} src={frameSrc} alt="frame" onLoad={() => setFrameStatus("ok")} onError={() => setFrameStatus("error")}/>
+                <Box
+                position="absolute" 
+                top="0" 
+                left="0" 
+                width="100%" 
+                height="100%" 
+                display={frameStatus === "pending" ? "grid" : "none"}
+                alignContent="center" 
+                justifyContent="center" 
+                sx={{backdropFilter: "blur(4px)"}}><CircularProgress/></Box>
+                <LinearProgress value={time / 20 - 35} variant="determinate"/>
+              </Box>
+              <Button 
+                variant="contained"
+                size="large"
+                onClick={resetFrame}>
+                Обновить изображение
+              </Button>
+              <Button 
+                variant="contained"
+                size="large"
+                onClick={resetFrame}>
+                Сделать снимок
+              </Button>
+              <Button 
+                variant="contained"
+                color="error"
+                size="large"
+                onClick={resetFrame}>
+                Брак
+              </Button>
+            </Stack>
+          </Grid>
+          <Grid item xs={4}>
           <Stack spacing={2} marginTop={4} alignItems="center">
-            <Typography variant="h2">Кадры</Typography>
-            <Box position="relative">
-              <Image width={600} height={450} src={frameSrc} alt="frame" onLoad={() => setFrameStatus("ok")} onError={() => setFrameStatus("error")}/>
-              <Box
-              position="absolute" 
-              top="0" 
-              left="0" 
-              width="100%" 
-              height="100%" 
-              display={frameStatus === "pending" ? "grid" : "none"}
-              alignContent="center" 
-              justifyContent="center" 
-              sx={{backdropFilter: "blur(4px)"}}><CircularProgress/></Box>
-              
-            </Box>
-            <Button 
-              variant="contained"
-              size="large"
-              onClick={() => { setFrameSrc(`https://loremflickr.com/600/450?random=${Math.random()}`); setFrameStatus("pending")}}>
-              Обновить изображение
-            </Button>
-            <Button 
-              variant="contained"
-              size="large"
-              onClick={() => { setFrameSrc(`https://loremflickr.com/600/450?random=${Math.random()}`); setFrameStatus("pending")}}>
-              Сделать снимок
-            </Button>
-            <Button 
-              variant="contained"
-              color="error"
-              size="large"
-              onClick={() => { setFrameSrc(`https://loremflickr.com/600/450?random=${Math.random()}`); setFrameStatus("pending")}}>
-              Брак
-            </Button>
-          </Stack>
+              <Typography variant="h3">Информация</Typography>
+              <Box position="relative">
+                <Image width={600} height={450} src={numberSrc} alt="number" onLoad={() => setNumberStatus("ok")} onError={() => setNumberStatus("error")}/>
+                <Box
+                position="absolute" 
+                top="0" 
+                left="0" 
+                width="100%" 
+                height="100%" 
+                display={numberStatus === "pending" ? "grid" : "none"}
+                alignContent="center" 
+                justifyContent="center" 
+                sx={{backdropFilter: "blur(4px)"}}><CircularProgress/></Box>
+                
+              </Box>
+              <Typography variant="body1">Распознанный номер вагона: </Typography>
+              <Typography variant="body1">Поставщик: </Typography>
+              <Typography variant="body1">Уровень доверия: </Typography>
+              <Button 
+                variant="contained"
+                color="primary"
+                size="large"
+                onClick={() => { setNumberSrc(`https://loremflickr.com/600/450?random=${Math.random()}`); setNumberStatus("pending")}}>
+                Скачать отчет
+              </Button>
+            </Stack>
+          </Grid>
           </Grid>
         </Container>
       </ThemeProvider>
