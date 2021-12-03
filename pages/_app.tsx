09f2efe500617896eb1ebe8cf624c303aec73b96
ekinohito <zhenyagur9@gmail.com>
2021-12-03 20:58:32 +0300

    style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+    <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat"
+          rel="stylesheet"
+        />
+  </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
